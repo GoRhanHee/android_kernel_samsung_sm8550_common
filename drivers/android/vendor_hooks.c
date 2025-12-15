BayLibre/android_kernel_common

ANDROID: mm: Add a vendor hook to modify the page flags before the page is added to the lru
Implement a vendor hook that identifies file pages associated with
foreground applications and set their PG_active flag. This guarantees
these pages are added to the active lru list.

Bug: 451273404
Change-Id: Ieb7d4bd1b9eddff7cacc5c10c8a407ab2f06f69c
Signed-off-by: Dang Jian <dangjian@honor.corp-partner.google.com>

drivers/android/vendor_hooks.c
@@ -661,6 +661,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_keep_reclaimed_folio);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_clear_reclaimed_folio);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_evict_folios_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_may_unmap_folio);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_folio_add_lru);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_folio_add_lru_folio_activate);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_fault_pre_folio_locked);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_folio_mapped);

GoRhanHee/android_kernel_samsung_sm8550_common

Bug: 467921212
(cherry picked from commit c2333b86b0d7f7bfd75fab8d9ad511d31ae81465)

drivers/android/vendor_hooks.c
@@ -548,6 +548,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_keep_reclaimed_page);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_clear_reclaimed_page);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_evict_pages_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mglru_should_abort_scan);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_lru_cache_add);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_lru_cache_add_page_activate);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_fault_pre_page_locked);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_page_mapped);