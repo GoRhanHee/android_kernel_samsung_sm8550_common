304ebb8eab9c282b3045fc5670a26cd529e8d5ae
GoRhanHee <gorhanhee01@gmail.com>
2026-04-02 00:21:04 +0900

    include: uapi:  fix sigaction error

--- include/uapi/asm-generic/signal.h
+++ include/uapi/asm-generic/signal.h
@@ -71,14 +71,15 @@ typedef unsigned long old_sigset_t;
 #define __ARCH_HAS_SA_RESTORER
 #endif
 
-#ifndef __KERNEL__
+#if !defined(__KERNEL__) && !defined(__sigaction_defined)
+#define __sigaction_defined
 struct sigaction {
-	__sighandler_t sa_handler;
-	unsigned long sa_flags;
+    __sighandler_t sa_handler;
+    unsigned long sa_flags;
 #ifdef SA_RESTORER
-	__sigrestore_t sa_restorer;
+    __sigrestore_t sa_restorer;
 #endif
-	sigset_t sa_mask;		/* mask last for extensibility */
+    sigset_t sa_mask;       /* mask last for extensibility */
 };
 #endif
 
